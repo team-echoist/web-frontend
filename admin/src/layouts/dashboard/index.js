647079ed25c4ecd760363a21513639a6b012e040
[IoT1]이수연 <dudgk0216@gmail.com>
2024-05-16 23:37:04 +0900

    로그인 구현될때까지 임시로 axiosinterceptor 토큰 하드코딩 세팅

--- admin/src/layouts/dashboard/index.js
+++ admin/src/layouts/dashboard/index.js
@@ -37,6 +37,7 @@ import GroupAddIcon from "@mui/icons-material/GroupAdd";
 import GroupIcon from "@mui/icons-material/Group";
 import AddCardIcon from "@mui/icons-material/AddCard";
 import ReportIcon from "@mui/icons-material/Report";
+import axios from "axios";
 
 function Dashboard() {
   const { sales, tasks } = reportsLineChartData;
