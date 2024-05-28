c96aedfe2f474fb15cc9e9adc498e4b96d7744a7
leesuyoen <dudgk0216@sqkcloud.com>
2024-05-28 15:00:58 +0900

    신고테이블 추가

---
+++ admin-refact/src/layouts/reports/index.jsx
@@ -0,0 +1,63 @@
+// 신고내역 관리 페이지
+
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import DataTable from "examples/Tables/DataTable";
+
+// Data
+import reportTableData from "layouts/reports/data/reportTableData";
+
+function Reports() {
+  const { columns, rows } = reportTableData();
+
+  return (
+ 
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox pt={6} pb={3}>
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  신고 리스트
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={{ columns, rows }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+        </Grid>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Reports;
--- admin-refact/src/routes.jsx
+++ admin-refact/src/routes.jsx
@@ -1,20 +1,38 @@
-
-import Login from "layouts/login";
+import SignIn from "layouts/authentication/sign-in";
+import SignUp from "layouts/authentication/sign-up";
+import Reports from "layouts/reports";
 
 // @mui icons
-import Icon from "@mui/material/Icon";
 import LoginIcon from '@mui/icons-material/Login';
+import ReportIcon from '@mui/icons-material/Report';
+
+
 
 const routes = [
   {
     type: "collapse",
-    name: "Login",
-    key: "login",
+    name: "Sign In",
+    key: "sign-in",
     icon: <LoginIcon/>,
-    route: "/login",
-    component: <Login />,
+    route: "/authentication/sign-in",
+    component: <SignIn />,
   },
-
+  {
+    type: "collapse",
+    name: "SignUp",
+    key: "signup",
+    icon: <></>,
+    route: "/signup",
+    component: <SignUp />,
+  },
+  {
+    type: "collapse",
+    name: "Reports",
+    key: "reports",
+    icon: <ReportIcon />,
+    route: "/reports",
+    component: <Reports />,
+  },
 ];
 
 export default routes;
