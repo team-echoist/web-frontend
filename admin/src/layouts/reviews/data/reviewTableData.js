4e88227167803e646b6018843235ab5c2f5a2d44
김정연 <jy.keem2468@gmail.com>
2024-05-28 10:31:46 +0900

    리뷰 페이지 디자인 적용

--- admin/src/layouts/reviews/data/reviewTableData.js
+++ admin/src/layouts/reviews/data/reviewTableData.js
@@ -1,69 +1,56 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/function-component-definition */
-/**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// Material Dashboard 2 React components
+import PropTypes from "prop-types";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-import MDAvatar from "components/MDAvatar";
 import MDBadge from "components/MDBadge";
 
-// Images
-import team2 from "assets/images/team-2.jpg";
-import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
+const Author = ({ name, email }) => (
+  <MDBox display="flex" alignItems="center" lineHeight={1}>
+    <MDBox ml={2} lineHeight={1}>
+      <MDTypography display="block" variant="button" fontWeight="medium">
+        {name}
+      </MDTypography>
+      <MDTypography variant="caption">{email}</MDTypography>
+    </MDBox>
+  </MDBox>
+);
+
+Author.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
+
+const Title = ({ title, description }) => (
+  <MDBox lineHeight={1} textAlign="left">
+    <MDTypography display="block" variant="caption" color="text" fontWeight="medium">
+      {title}
+    </MDTypography>
+    <MDTypography variant="caption">{description}</MDTypography>
+  </MDBox>
+);
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+};
 
 export default function data() {
-  const Author = ({ image, name, email }) => (
-    <MDBox display="flex" alignItems="center" lineHeight={1}>
-      <MDAvatar src={image} name={name} size="sm" />
-      <MDBox ml={2} lineHeight={1}>
-        <MDTypography display="block" variant="button" fontWeight="medium">
-          {name}
-        </MDTypography>
-        <MDTypography variant="caption">{email}</MDTypography>
-      </MDBox>
-    </MDBox>
-  );
-
-  const Title = ({ title, description }) => (
-    <MDBox lineHeight={1} textAlign="left">
-      <MDTypography display="block" variant="caption" color="text" fontWeight="medium">
-        {title}
-      </MDTypography>
-      <MDTypography variant="caption">{description}</MDTypography>
-    </MDBox>
-  );
-
   return {
     columns: [
-      { Header: "제목", accessor: "title", width: "45%", align: "left" },
+      { Header: "내용", accessor: "title", width: "45%", align: "left" },
       { Header: "작성자", accessor: "name", width: "45%", align: "left" },
-      { Header: "신고 여부", accessor: "status", align: "center" },
+      { Header: "관리자 댓글여부", accessor: "status", align: "center" },
       { Header: "작성일", accessor: "registrationDate", align: "center" },
       { Header: "관리", accessor: "action", align: "center" },
     ],
 
     rows: [
       {
-        title: <Title title="오늘은 쟁반짜장을 먹었습니다." />,
-        name: <Author image={team2} name="John Michael" email="john@creative-tim.com" />,
+        title: <Title title="앱 직이네예" description="" />,
+        name: <Author name="John Michael" email="john@creative-tim.com" />,
         status: (
           <MDBox ml={-1}>
-            <MDBadge badgeContent="Subscribe" color="success" variant="gradient" size="sm" />
+            <MDBadge badgeContent="YES" color="success" variant="gradient" size="sm" />
           </MDBox>
         ),
         registrationDate: (
@@ -78,11 +65,11 @@ export default function data() {
         ),
       },
       {
-        title: <Title title="Diary2" />,
-        name: <Author image={team3} name="Alexa Liras" email="alexa@creative-tim.com" />,
+        title: <Title title="제 일생일대 일기를 링크드아웃덕분에 써보았습니다." description="" />,
+        name: <Author name="Alexa Liras" email="alexa@creative-tim.com" />,
         status: (
           <MDBox ml={-1}>
-            <MDBadge badgeContent="Normal user" color="dark" variant="gradient" size="sm" />
+            <MDBadge badgeContent="NO" color="dark" variant="gradient" size="sm" />
           </MDBox>
         ),
         registrationDate: (
