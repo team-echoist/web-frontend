9bf8f8493c196abd176e5ba254e49d768ba645e9
leesuyoen <dudgk0216@sqkcloud.com>
2024-05-28 11:14:14 +0900

    defaultlayout 초기 임시세팅

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,23 @@
 "use client";
+import { useEffect, useState } from "react";
 import { Main } from "@/pages-flat/main";
 import { OnBoarding } from "@/pages-flat/onboarding";
 import { useMediaQuery } from "@/shared/lib/media";
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
   const isDesktop = useMediaQuery("(min-width: 768px)");
-  // 추후 구체적인 사이즈 나오면 제설정 예정
-  return isDesktop ? <OnBoarding /> : <Main />;
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return <Main />;
+  }
+  // 브라우저가 렌더링 된후에만 isDestop 값을 설정하고, 아닐경우에는 하나의 컴포먼트만 고정되게 렌더링 되게 한다.
+  // 추후 구체적인 사이즈 나오면 재설정 예정
+  // return isDesktop ? <OnBoarding /> : <Main />;
+  return  <Main />
+  // 현재 웹 온보딩 디자인이 안나왔기때문에 앱기준으로 개발 시작
 }
