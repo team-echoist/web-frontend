bb2b25b0134489d6bfc8a31d3e7157d348d73d30
김정연 <jy.keem2468@gmail.com>
2024-05-30 17:36:36 +0900

    added justand

--- web/src/shared/store/index.ts
+++ web/src/shared/store/index.ts
@@ -1,1 +1,9 @@
-// 저스텐드 store 세팅할자리
+import { create } from "zustand"; // create로 zustand를 불러옵니다.
+
+const useStore = create((set) => ({
+  bears: 0,
+  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+}));
+
+export default useStore;
