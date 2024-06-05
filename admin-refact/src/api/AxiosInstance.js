5304fc5f009ca01f5f452f7441458b8ae8c1c23d
김정연 <jy.keem2468@gmail.com>
2024-06-05 23:28:09 +0900

    axiosInstance에서 token 갱신 코드 추가

--- admin-refact/src/api/AxiosInstance.js
+++ admin-refact/src/api/AxiosInstance.js
@@ -14,4 +14,21 @@ AxiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
+AxiosInstance.interceptors.response.use((response) => {
+  const newToken = response.headers["Authorization"];
+  if (newToken) {
+    const tokenValue = newToken.split(" ");
+    const oldToken = localStorage.getItem("token");
+
+    if (oldToken != tokenValue) {
+      console.log(
+        `토큰 갱신 완룡, 이전 토큰, ${oldToken} 뉴 토큰~ ${tokenValue}`
+      );
+
+      localStorage.setItem("token", tokenValue);
+    }
+    return response;
+  }
+});
+
 export default AxiosInstance;
