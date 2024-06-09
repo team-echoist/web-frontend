3dad90c466e7c8221042a5df3de5e92c78789267
leesuyoen <dudgk0216@naver.com>
2024-06-09 21:00:07 +0900

    이메일 쿠키 담는로직 복구

--- admin-refact/src/layouts/profile/api/index.js
+++ admin-refact/src/layouts/profile/api/index.js
@@ -4,3 +4,12 @@ export const fetchProfileData = (id) =>{
     const response = AxiosInstance.get("/api/v1/users/profile");
 }
 
+export const fetchAdminList = async() =>{
+    const response =await AxiosInstance.get("/admin",{
+        params:{
+            active:true
+        }
+    })
+
+    
+}
