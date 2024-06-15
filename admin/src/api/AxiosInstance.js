9fe0442ce9f60ebae90524e6eba4de5d1e166c4b
leesuyoen <dudgk0216@naver.com>
2024-06-15 21:23:12 +0900

    어드민 폴더 삭제및 admin-recat폴더 네이밍 변경

--- admin/src/api/AxiosInstance.js
+++ admin/src/api/AxiosInstance.js
@@ -1,14 +1,38 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + "/api/",
+  baseURL: import.meta.env.VITE_ROOT_API_URL,
 });
 
 AxiosInstance.interceptors.request.use((config) => {
-  const tempToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJhZG1pbjFAbGlua2Vkb3V0YXBwLmNvbSIsImlhdCI6MTcxNTg2OTkzNywiZXhwIjoxNzE1OTU2MzM3fQ._5goGeo_bccmtF20aTu20770ZX86pwWmvVbz5QBXiOo";
-  // token 오는거 보고 재세팅
-  config.headers.Authorization = `Bearer ${tempToken}`;
+  const token = Cookies.get("token");
+  if (token) {
+    config.headers.Authorization = `${token}`;
+  }
+  return config;
 });
+
+// 응답 인터셉터: 서버로부터의 모든 응답 후에 실행
+AxiosInstance.interceptors.response.use(
+  (response) => {
+    const newToken = response.headers["Authorization"];
+    if (newToken) {
+      const tokenValue = newToken.split(" ")[1];
+      Cookies.set("token", tokenValue, { secure: true,  sameSite: "Strict" });
+    }
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// // 토큰을 인위적으로 만료시키는 함수(테스트용)
+// const expireToken = () => {
+//   Cookies.remove("token");
+// };
+
+// setTimeout(expireToken, 10000);
 
 export default AxiosInstance;
