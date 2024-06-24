a9ac4cc480ac474c3eca1bb6c85d37ced438374f
leesuyoen <dudgk0216@sqkcloud.com>
2024-06-24 17:24:12 +0900

    텍스트 에디터 세팅

--- admin/src/layouts/update/index.jsx
+++ admin/src/layouts/update/index.jsx
@@ -1,10 +1,55 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useState } from "react";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import MDEditor from "@uiw/react-md-editor";
 
 function index() {
+  const [value, setValue] = useState("**Hello world!!!**");
   return (
     <DashboardLayout>
       <DashboardNavbar />
+      <Box
+        sx={{
+          flexGrow: 1,
+          width: "100%",
+          minHeight: "50rem",
+          marginTop: "20px",
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Box
+              sx={{
+                width: "100%",
+                height: "100%",
+                paddingLeft: "20px",
+              }}
+            >
+              미리보기 자리
+            </Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Box
+              sx={{
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              <MDEditor
+                value={value}
+                onChange={setValue}
+                height={800}
+                preview="edit"
+              />
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+      <Footer />
     </DashboardLayout>
   );
 }
