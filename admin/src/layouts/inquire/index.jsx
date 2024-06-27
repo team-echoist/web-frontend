e8598bf64b8a6c5d0487099b426d2ffe7dc5146d
leesuyoen <dudgk0216@sqkcloud.com>
2024-06-27 13:51:25 +0900

    테이블 문구 변경

--- admin/src/layouts/inquire/index.jsx
+++ admin/src/layouts/inquire/index.jsx
@@ -1,15 +1,26 @@
+import { useState, useEffect } from "react";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import inquireTableData from "components/Tables/data/inquireTableData";
 import Tables from "components/Tables";
+import Footer from "examples/Footer";
+import { Pagination,Box } from "@mui/material";
 
 function index() {
   const { columns, rows } = inquireTableData();
+  const [currentPage, setCurrentPage] = useState(1);
+  const rowsPerPage = 10;
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
-      <Tables title="Notice" columns={columns} rows={rows} />
+      <Tables title="Inquire" columns={columns} rows={rows} />
+      <Box display="flex" justifyContent="center" p={2}>
+      <Pagination  color="secondary"
+          sx={{ color: "white" }}/>
+             </Box>
+      <Footer />
+      
     </DashboardLayout>
   );
 }
