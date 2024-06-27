0e6c24bd0a79025e62c3bce5e56125c6b1e3b4e2
leesuyoen <dudgk0216@sqkcloud.com>
2024-06-27 10:22:18 +0900

    데이터 명칭변경

---
+++ admin/src/layouts/inquire/index.jsx
@@ -0,0 +1,10 @@
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+
+function index() {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
