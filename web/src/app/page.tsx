5ddc2695dbf97d98c656d197efc17e3a55ad2f51
leesuyoen <dudgk0216@sqkcloud.com>
2024-07-04 17:47:28 +0900

    오타수정

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { OnBoarding } from "@/pages-flat/onboarding";
-import { WebOnboarding } from "@/pages-flat/web_onboarding";
+import { WebOnBoarding } from "@/pages-flat/web_onboarding";
 import { useMediaQuery } from "@/shared/lib/media";
 import { useStore } from "@/shared/store";
 import styled from "styled-components";
@@ -27,7 +27,7 @@ export default function Home() {
   }
   // 브라우저가 렌더링 된후에만 isDestop 값을 설정하고, 아닐경우에는 하나의 컴포먼트만 고정되게 렌더링 되게 한다.
   // 추후 구체적인 사이즈 나오면 재설정 예정
-  // return isDesktop ? <WebOnboarding /> : <Main />;
+  // return isDesktop ? <WebOnBoarding /> : <Main />;
   return (
     <Container>
       <OnBoarding />
