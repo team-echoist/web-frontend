f2be860ed3441c5a693cd3386d504fa95b0f540e
leesuyoen <dudgk0216@sqkcloud.com>
2024-07-08 15:52:32 +0900

    jest react testing library 초기 세팅

---
+++ web/jest.config.ts
@@ -0,0 +1,18 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
+
+export default createJestConfig(customJestConfig);
