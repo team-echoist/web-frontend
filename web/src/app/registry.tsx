9e7e2d291a97cba6ae104a32641a2502db9bf622
jungyeon-rgb <jy.keem2468@gmail.com>
2024-07-15 09:52:07 +0900

    font 전역 적용

--- web/src/app/registry.tsx
+++ web/src/app/registry.tsx
@@ -1,31 +1,29 @@
-"use client";
+'use client'
 
-import React, { useState } from "react";
-import { useServerInsertedHTML } from "next/navigation";
-import { ServerStyleSheet, StyleSheetManager } from "styled-components";
-import GlobalStyleComponent from "@/app/GlobalStyle";
+import React, { useState } from 'react'
+import { useServerInsertedHTML } from 'next/navigation'
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
+import GlobalStyleComponent from '@/app/GlobalStyle'
+import GlobalStyle from '@/shared/styles/GlobalStyles'
 
-export default function StyledComponentsRegistry({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
+    const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
 
-  useServerInsertedHTML(() => {
-    const styles = styledComponentsStyleSheet.getStyleElement();
-    styledComponentsStyleSheet.instance.clearTag();
-    return <>{styles}</>;
-  });
+    useServerInsertedHTML(() => {
+        const styles = styledComponentsStyleSheet.getStyleElement()
+        styledComponentsStyleSheet.instance.clearTag()
+        return <>{styles}</>
+    })
 
-  if (typeof window !== "undefined") return <>{children}</>;
+    if (typeof window !== 'undefined') return <>{children}</>
 
-  return (
-    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
-      <GlobalStyleComponent />
-      {children}
-    </StyleSheetManager>
-  );
+    return (
+        <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+            <GlobalStyleComponent />
+            <GlobalStyle />
+            {children}
+        </StyleSheetManager>
+    )
 }
 
 // next js는 모든 스타일을 수집하고 <head> </head>태그에 적용하는 전역 스타일 레지스트리 컴포넌트를 구현하는것을 권장함
