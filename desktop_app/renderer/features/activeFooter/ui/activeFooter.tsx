a48ea1ca284113eb64568343028aa5409568238a
leesuyoen <dudgk0216@sqkcloud.com>
2024-08-08 17:00:45 +0900

    푸터 작업중

---
+++ desktop_app/renderer/features/activeFooter/ui/activeFooter.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { Footer } from "@/shared/ui/footer";
+
+function ActiveFooter() {
+  return <Footer />;
+}
+
+export default ActiveFooter;
--- desktop_app/renderer/pages-flat/main/ui/main.tsx
+++ desktop_app/renderer/pages-flat/main/ui/main.tsx
@@ -3,29 +3,33 @@ import React from "react";
 import styled from "styled-components";
 import { AlarmButton } from "@/shared/ui/button";
 import { CircleButton } from "@/shared/ui/button";
-import WriteIcon from "@/shared/assets/img/write_icon.svg"
-import HomeImg from "@/shared/assets/img/mainroom.webp"
+import WriteIcon from "@/shared/assets/img/write_icon.svg";
+import HomeImg from "@/shared/assets/img/mainroom.webp";
 import Image from "next/image";
+import ActiveFooter from "@/features/activeFooter/ui/activeFooter";
 
 const Container = styled.main`
-width:82.8vw;
-font-family: Arial, sans-serif;
-position:fixed;
-top:32px;
+  width: 82.8vw;
+  font-family: Arial, sans-serif;
+  position: fixed;
+  top: 32px;
 `;
-const HomeDiv =styled.div`
-width: 100%;
-height: 89.6vh;
-`
+const HomeDiv = styled.div`
+  width: 100%;
+  height: 88.6vh;
+`;
 
 export const Main = () => {
   return (
-    <Container>
-      <HomeDiv>
-      <Image alt="home" src={HomeImg} fill/>
-      </HomeDiv>
-      {/* <AlarmButton></AlarmButton>
+    <>
+      <Container>
+        <HomeDiv>
+          <Image alt="home" src={HomeImg} fill />
+        </HomeDiv>
+        {/* <AlarmButton></AlarmButton>
       <WriteIcon></WriteIcon> */}
-    </Container>
+      </Container>
+      <ActiveFooter/>
+    </>
   );
 };
