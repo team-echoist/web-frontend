af0bdc6d4c7d84ac4dc94cd6cbe876d76120ee5f
leesuyoen <dudgk0216@sqkcloud.com>
2024-08-22 16:45:34 +0900

    에디터 공통 컴포넌트 세팅중

--- desktop_app/renderer/pages-flat/write_essay/ui/writeessay.tsx
+++ desktop_app/renderer/pages-flat/write_essay/ui/writeessay.tsx
@@ -1,6 +1,15 @@
 import React from "react";
-import * as Styled from "./writeessay.styled";
+import styled from "styled-components";
+import { Editor } from "@/shared/ui/editor";
+
+const Layout = styled.main`
+  margin-top: 100px;
+`;
 
 export const WriteEssay = () => {
-  return <Styled.SLayout>WriteEssay</Styled.SLayout>;
+  return (
+    <Layout>
+      <Editor />
+    </Layout>
+  );
 };
---
+++ desktop_app/renderer/shared/ui/editor/Editor.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactQuill, { Quill } from "react-quill";
+
+function Editor() {
+  return (
+    <>
+      <ReactQuill />
+    </>
+  );
+}
+
+export default Editor;
