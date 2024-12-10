a06d28ecbba370ced5886e26ee1e1ad8c873da6c
leesuyoen <dudgk0216@sqkcloud.com>
2024-12-10 16:48:42 +0900

    프로필 navigate 함수 버그 수정

--- desktop_app/renderer/features/showessaydetails/ui/contents/UserProfile.tsx
+++ desktop_app/renderer/features/showessaydetails/ui/contents/UserProfile.tsx
@@ -126,7 +126,7 @@ function UserProfile({
     submitFollows(isFollow);
   };
   const navigateUserProfile = (id: number) => {
-    router.push(`/web/user_profile?id=${id}&isSubscribed=true`);
+    router.push(`/web/user_profile?id=${id}&isSubscribed=${isFollow}`);
   };
 
   return (
