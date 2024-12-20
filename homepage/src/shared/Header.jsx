74673ad3ada9fd6ff3a1547b202ba1ed64fcc733
jungyeon <jy.keem2468@gmail.com>
2024-12-20 16:31:15 +0900

    premium 페이지 이동 금지 및 alert출력

--- homepage/src/shared/Header.jsx
+++ homepage/src/shared/Header.jsx
@@ -11,29 +11,34 @@ const Header = () => {
 
     const linkClasses = (path) => `mx-[51px] my-[52px] p-2 font-normal ${activePath === path ? "text-white" : "text-[#919191]"}`;
 
+    const handlePremiumClick = (event) => {
+        event.preventDefault();
+        alert("서비스 준비중입니다.");
+    };
+
     return (
-        <nav className="flex bg-[#0F0F0F] justify-between items-center text-[20px] text-[#919191] px-10 font-normal">
-            <Link to="/">
-                <img src="/images/logo.svg" alt="로고" width={154} height={63.179} className="py-[19.744px]" />
-            </Link>
-            <div>
-                <Link to="/about" className={linkClasses("/about")}>
-                    About
+            <nav className="flex bg-[#0F0F0F] justify-between items-center text-[20px] text-[#121212] px-10 font-normal">
+                <Link to="/">
+                    <img src="/images/logo.svg" alt="로고" width={154} height={63.179} className="py-[19.744px]" />
                 </Link>
-                <Link to="/learn" className={linkClasses("/learn")}>
-                    Learn
-                </Link>
-                <Link to="/premium" className={linkClasses("/premium")}>
-                    Premium
-                </Link>
-                <Link to="/newsletter" className={linkClasses("/newsletter")}>
-                    Newsletter
-                </Link>
-            </div>
-            <p className="w-[143px] h-[46px] rounded-2xl bg-white text-[#121212] flex justify-center items-center font-normal">
-                Get the App
-            </p>
-        </nav>
+                <div>
+                    <Link to="/about" className={linkClasses("/about")}>
+                        About
+                    </Link>
+                    <Link to="/learn" className={linkClasses("/learn")}>
+                        Learn
+                    </Link>
+                    <Link to="/premium" onClick={handlePremiumClick} className={linkClasses("/premium")}>
+                        Premium
+                    </Link>
+                    <Link to="/newsletter" className={linkClasses("/newsletter")}>
+                        Newsletter
+                    </Link>
+                </div>
+                <p className="w-[143px] h-[46px] rounded-2xl bg-white text-[#121212] flex justify-center items-center font-normal">
+                    Get the App
+                </p>
+            </nav>
     );
 }
 
