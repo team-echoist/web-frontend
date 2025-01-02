6e0096ba2777bfcad8d7908b6759528677c22e54
leesuyoen <dudgk0216@naver.com>
2025-01-02 15:24:58 +0900

    빌드 에러 재수정

--- desktop_app/renderer/pages/GlobalStyle.ts
+++ desktop_app/renderer/pages/GlobalStyle.ts
@@ -10,7 +10,7 @@ const slideUp = keyframes`
   }
 `;
 
-const GlobalStyleComponent = createGlobalStyle<{ isAppleIdPage: boolean }>`
+const GlobalStyleComponent = createGlobalStyle`
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
