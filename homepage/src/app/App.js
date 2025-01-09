94705f53a61525cc17a73bb328c7a311e92a4eb2
leesuyoen <dudgk0216@naver.com>
2025-01-09 19:58:44 +0900

    불필요한 코드 정리및 페이지 전환시 스크롤 상단에 위치하게

--- homepage/src/app/App.js
+++ homepage/src/app/App.js
@@ -1,9 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { useLayoutEffect } from "react";
 import routes from "./routes";
+function ScrollToTop() {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         {routes.map((route, index) => {
           if (route.route && route.component) {
