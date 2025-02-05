cec7bc334dba6ed8212d764c78a2316aa38881cc
leesuyoen <dudgk0216@naver.com>
2025-02-05 18:06:15 +0900

    홈페이지 메인 문구 레이아웃 수정

--- homepage/src/pages/main/contents/web/WebDesc.jsx
+++ homepage/src/pages/main/contents/web/WebDesc.jsx
@@ -18,12 +18,12 @@ function WebDesc() {
           src={"/images/mainpage/highlight.png"}
           alt="highlights"
         />
-        <p className="absolute xl:top-[35px] lg:top-[18px] md:top-[18px] xl:left-[13%] lg:left-[28%] md:left-[18%]  text-white text-center font-['Pretendard'] xl:text-[24.402px] lg:text-[15.175px] md:text-[15.175px] font-thin leading-[39.043px] tracking-[-0.732px]">
-          :Link
-        </p>
-        <p className="absolute xl:top-[35px] lg:top-[18px] md:top-[18px] xl:left-[40%] lg:left-[85%]  md:left-[85%] text-white text-center font-['Pretendard']  xl:text-[24.402px] lg:text-[15.175px] font-thin leading-[39.043px] tracking-[-0.732px]">
-          :Out
-        </p>
+        <p className="absolute xl:top-[2rem] lg:top-[1rem] md:top-[1rem] xl:left-[15%] lg:left-[38%] md:left-[35%] text-white text-center font-pretendard xl:text-[1.5rem] lg:text-[1rem] font-thin leading-[2rem] tracking-[-0.05rem]">
+        :Link
+      </p>
+      <p className="absolute xl:top-[2rem] lg:top-[1rem] md:top-[1rem] xl:left-[45%] lg:left-[85%] md:left-[85%] text-white text-center font-pretendard xl:text-[1.5rem] lg:text-[1rem] font-thin leading-[2rem] tracking-[-0.05rem]">
+        :Out
+      </p>
         <p className="text-white xl:w-[503px] lg:w-[406px] md:w-[306px] break-all xl:static xl2:absolute xl2:top-[-10px] xl2:left-[530px] lg:absolute lg:top-[-10px] lg:left-[350px] md:absolute md:top-[-10px] md:left-[350px]">
           링크드아웃에서는 '다양한 관계 속의 나'를 한발짝 떨어져 들여다보는
           기회를 글쓰기 경험을 통해 제공하고 있어요.
